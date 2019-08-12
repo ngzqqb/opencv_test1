7c9a99ce9bb68b80da637e3e78d6e1348ffa41ba
ngzHappy <zhaixueqiang@hotmail.com>
2019-08-12 23:15:06 +0800

    Update MainWindow.cpp

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -5,7 +5,7 @@
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
-    ui(new Ui::MainWindow){
+    ui(new Ui::MainWindow) {
     ui->setupUi(this);
 }
 
@@ -14,9 +14,9 @@ MainWindow::~MainWindow() {
 }
 
 
-void MainWindow::on_imageFileNameButton_clicked(){
-    auto varAns = QFileDialog::getOpenFileName(nullptr,{},ui->imageFileName->text());
-    if(varAns.isEmpty()){
+void MainWindow::on_imageFileNameButton_clicked() {
+    auto varAns = QFileDialog::getOpenFileName(nullptr, {}, ui->imageFileName->text());
+    if (varAns.isEmpty()) {
         return;
     }
     ui->imageFileName->setText(varAns);
@@ -28,28 +28,19 @@ public:
     double thisLineLength;
     double thisLineAngle;
     inline LineItem(const QLineF & arg) :
-        QLineF{arg},
-        thisLineLength{arg.length()},
-        thisLineAngle{arg.angle()} {
-        thisLineAngle = std::fmod( thisLineAngle , 360. );
-        if(thisLineAngle<360.){
-            thisLineAngle+=360 ;
-        }
-        assert(thisLineAngle>=0);
-        thisLineAngle =
-        std::min({thisLineAngle,
-                  std::abs(90-thisLineAngle),
-                  std::abs(180-thisLineAngle),
-                  std::abs(360-thisLineAngle)});
+        QLineF{ arg },
+        thisLineLength{ arg.length() },
+        thisLineAngle{ arg.angle() } {
+        thisLineAngle = std::abs(std::fmod(thisLineAngle, 45.));
     }
-    inline LineItem():
-        thisLineLength{0},
-        thisLineAngle{0}{
+    inline LineItem() :
+        thisLineLength{ 0 },
+        thisLineAngle{ 0 }{
     }
-    inline LineItem(const LineItem &)=default;
-    inline LineItem(LineItem &&)=default;
-    inline LineItem&operator=(const LineItem &)=default;
-    inline LineItem&operator=(LineItem &&)=default;
+    inline LineItem(const LineItem &) = default;
+    inline LineItem(LineItem &&) = default;
+    inline LineItem&operator=(const LineItem &) = default;
+    inline LineItem&operator=(LineItem &&) = default;
 };
 
 void MainWindow::on_callButton_clicked() {
@@ -60,36 +51,36 @@ void MainWindow::on_callButton_clicked() {
     std::vector< LineItem > varLines;
     {
         std::vector<cv::Vec4i> lines;
-        cv::HoughLinesP( varCannyImage,
-                         lines,
-                         1.05/*半径分辨率*/,
-                         CV_PI / 180/*角度分辨率*/,
-                         30/*直线点数阈值*/,
-                         120/*直线长度阈值*/,
-                         16/*最大距离*/);
-        QImage varImage{  varCannyImage.cols,varCannyImage.rows,QImage::Format_RGBA8888 };
-        varImage.fill( QColor(255,255,255,255) );
+        cv::HoughLinesP(varCannyImage,
+            lines,
+            1.05/*半径分辨率*/,
+            CV_PI / 180/*角度分辨率*/,
+            30/*直线点数阈值*/,
+            120/*直线长度阈值*/,
+            16/*最大距离*/);
+        QImage varImage{ varCannyImage.cols,varCannyImage.rows,QImage::Format_RGBA8888 };
+        varImage.fill(QColor(255, 255, 255, 255));
         varLines.reserve(lines.size());
         {
             qDebug() << lines.size();
-            QPainter varPainter{&varImage};
-            varPainter.drawImage(QPoint{0,0}, QImage{varFileName} );
-            varPainter.setPen({QColor{255,0,0},2 });
-            for(const auto & varI : lines){
+            QPainter varPainter{ &varImage };
+            varPainter.drawImage(QPoint{ 0,0 }, QImage{ varFileName });
+            varPainter.setPen({ QColor{255,0,0},2 });
+            for (const auto & varI : lines) {
                 QLineF varLine{ QPoint{ varI[0],varI[1] },QPoint{ varI[2],varI[3] } };
                 varPainter.drawLine(varLine);
                 varLines.emplace_back(varLine);
             }
         }
-        varImage.save(varFileName+QStringLiteral(".jpg"));
+        varImage.save(varFileName + QStringLiteral(".jpg"));
     }
     //sstd::saveImage(varCannyImage,varFileName+QStringLiteral(".jpg"));
-    std::sort( varLines.begin(),varLines.end(),[](const auto & l,const auto & r){return l.thisLineLength>r.thisLineLength;} );
-    if(varLines.size()>128){
+    std::sort(varLines.begin(), varLines.end(), [](const auto & l, const auto & r) {return l.thisLineLength > r.thisLineLength; });
+    if (varLines.size() > 128) {
         varLines.resize(128);
     }
-    for(const auto & varI : varLines){
-        qDebug() << varI.thisLineAngle ;
+    for (const auto & varI : varLines) {
+        qDebug() << varI.thisLineAngle;
     }
 }
 
