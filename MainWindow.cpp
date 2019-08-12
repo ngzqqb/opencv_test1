599c561c8f0f17d8a4798215774f29aed43b86d5
ngzHappy <zhaixueqiang@hotmail.com>
2019-08-12 22:29:56 +0800

    Update MainWindow.cpp

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -30,9 +30,23 @@ void MainWindow::on_callButton_clicked() {
    std::vector<cv::Vec4i> lines;
    cv::HoughLinesP( varCannyImage,
                     lines,
-                    1/*半径分辨率*/,
-                    CV_PI / 72/*角度分辨率*/,
-                    300/*线段长度阈值*/ );
-   sstd::saveImage(varCannyImage,varFileName+QStringLiteral(".jpg"));
+                    1.05/*半径分辨率*/,
+                    CV_PI / 180/*角度分辨率*/,
+                    30/*直线点数阈值*/,
+                    120/*直线长度阈值*/,
+                    16/*最大距离*/);
+   QImage varImage{  varCannyImage.cols,varCannyImage.rows,QImage::Format_RGBA8888 };
+   varImage.fill( QColor(255,255,255,255) );
+   {
+       qDebug() << lines.size();
+       QPainter varPainter{&varImage};
+       varPainter.drawImage(QPoint{0,0}, QImage{varFileName} );
+       varPainter.setPen({QColor{255,0,0},2 });
+       for(const auto & varI : lines){
+           varPainter.drawLine(QPoint{ varI[0],varI[1] },QPoint{ varI[2],varI[3] });
+       }
+   }
+   varImage.save(varFileName+QStringLiteral(".jpg"));
+   //sstd::saveImage(varCannyImage,varFileName+QStringLiteral(".jpg"));
 }
 
