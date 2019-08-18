a9c1536cbc175edd27199fc56284d22904f99d51
ngzHappy <zhaixueqiang@hotmail.com>
2019-08-18 10:52:32 +0800

    +update

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -22,29 +22,8 @@ void MainWindow::on_imageFileNameButton_clicked() {
     ui->imageFileName->setText(varAns);
 }
 
-
-class LineItem : public QLineF {
-public:
-    double thisLineLength;
-    double thisLineAngle;
-    inline LineItem(const QLineF & arg) :
-        QLineF{ arg },
-        thisLineLength{ arg.length() },
-        thisLineAngle{ arg.angle() } {
-        thisLineAngle = std::abs(std::fmod(thisLineAngle, 45.));
-    }
-    inline LineItem() :
-        thisLineLength{ 0 },
-        thisLineAngle{ 0 }{
-    }
-    inline LineItem(const LineItem &) = default;
-    inline LineItem(LineItem &&) = default;
-    inline LineItem&operator=(const LineItem &) = default;
-    inline LineItem&operator=(LineItem &&) = default;
-};
-
 void MainWindow::on_callButton_clicked() {
     auto varFileName = ui->imageFileName->text();
-    sstd::evalAngle(varFileName);
+    qDebug() << sstd::evalAngle(varFileName);
 }
 
